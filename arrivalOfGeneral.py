c0ca0512d7ff83cda0436d86b01780c01bec9e98
Edula23 <edengebeta210@gmail.com>
2025-03-31 10:52:05 -0400

    Arrival of the general problem solution with python

---
+++ arrivalOfGeneral.py
@@ -0,0 +1,27 @@
+size = int(input())
+n = list(map(int, input().split()))
+
+
+counter = 0
+for i in n:
+    maxIndex = n.index(max(n))
+    if maxIndex==0:
+        break
+    else:
+        n[maxIndex], n[maxIndex-1]=n[maxIndex-1], n[maxIndex]
+        counter+=1
+
+
+for i in n:
+    minimum = min(n)
+    minIndex = n.index(minimum)
+    for i in range(size):
+        if n[i]==minimum:
+            minIndex=i
+    if minIndex==size-1:
+        break
+    else:
+        n[minIndex], n[minIndex+1]=n[minIndex+1], n[minIndex]
+        counter+=1
+
+print(counter)
